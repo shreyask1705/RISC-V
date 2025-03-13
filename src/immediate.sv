594bf6a2374fb1ab2d9fde750c66abfb257273d6
shreyask1705 <khairnarshreyash@iitbhilai.ac.in>
2025-03-13 11:35:04 +0530

    Added Jump logic and renamed

--- src/immediate.sv
+++ src/immediate.sv
@@ -7,14 +7,17 @@ module immediate (
     reg [31:0] immediate_extended_reg;
     always_comb begin : immediate_comb
         case (ImmSrc) 
-            2'b00 : begin
+            2'b00 : begin // I-type
                 immediate_reg = {instruction[31], instruction[31:20]};
             end
-            2'b01 : begin
+            2'b01 : begin // Store 
                 immediate_reg = {instruction[31],instruction[31:25], instruction[11:7]};
             end
-            2'b10 : begin
+            2'b10 : begin // Branch
                 immediate_reg = {instruction[31], instruction[7], instruction[30:25], instruction[11:8], 1'b0};
+            end
+            2'b11 : begin // Jump
+                immediate_reg = {instruction[31], instruction[19:12], instruction[20], instruction[30:21], 1'b0};
             end
             default : begin 
                 immediate_reg = 20'b0;
